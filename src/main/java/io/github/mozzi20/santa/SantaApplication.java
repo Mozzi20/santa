1174e2c387fb7a2d0bb5702120ff39daaa79470a
Movitz Sunar <movitz.sunar@gmail.com>
2019-11-02 23:55:24 +0100

    removed sentry dsn

--- src/main/java/io/github/mozzi20/santa/SantaApplication.java
+++ src/main/java/io/github/mozzi20/santa/SantaApplication.java
@@ -1,7 +1,9 @@
 package io.github.mozzi20.santa;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
@@ -16,8 +18,9 @@ public class SantaApplication {
 	}
 	
 	@Bean
-	public HandlerExceptionResolver sentryExceptionResolver() {
-		Sentry.init("https://91a9ca21c5e740b29fff2653cf31d5b6@sentry.io/1805760");
+	@ConditionalOnProperty("sentry.dsn")
+	public HandlerExceptionResolver sentryExceptionResolver(@Value("${sentry.dsn}") String dsn) {
+		Sentry.init(dsn);
 	    return new SentryExceptionResolver();
 	}
 
